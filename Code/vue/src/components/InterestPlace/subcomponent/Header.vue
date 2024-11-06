02d2f4daab7e67e8d79fab7b91673d412f65b6fd
Lenovo <15366911568@163.com>
2024-11-06 16:57:09 +0800

    PlaceDetail.vue change(分析页)

--- Code/vue/src/components/InterestPlace/subcomponent/Header.vue
+++ Code/vue/src/components/InterestPlace/subcomponent/Header.vue
@@ -1,20 +1,22 @@
 <template>
   <div class="button-container">
-    <el-page-header :icon="ArrowLeft" title="返回"   @click="$router.go(-1)" >
+    <!-- 返回按钮和标题 -->
+    <el-page-header @back="onBack" class="header">
       <template #content>
-        <span class="text-large font-600 mr-3" style="font-size: larger"> Title </span>
+        <span class="title">返回</span>
       </template>
     </el-page-header>
 
-    <div>
-      <div class="sub-title">用户名</div>
-        <div class="block">
-          <el-avatar :size="50" :src="circleUrl" />
-        </div>
+    <!-- 用户信息和头像 -->
+    <div class="user-info">
+      <img src="@/assets/cat.gif" style="width: 100px" @click="drawer = true">
+      <div class="block">
+        <el-avatar :size="50" :src="circleUrl" />
+      </div>
       <div class="toolbar">
         <el-dropdown>
-          <el-icon style="margin-right: 0px;font-size:25px;margin-top: 5px">
-            <setting />
+          <el-icon style="margin-right: 0px; font-size: 25px; margin-top: 5px">
+            <Setting />
           </el-icon>
           <template #dropdown>
             <el-dropdown-menu>
@@ -26,22 +28,85 @@
         </el-dropdown>
       </div>
     </div>
-    </div>
-
+    <el-drawer v-model="drawer" title="I am the title" :with-header="false">
+      <span>Hi there!</span>
+    </el-drawer>
+  </div>
 </template>
 
 <script setup>
+import { ArrowLeft, Setting } from "@element-plus/icons-vue";
+import { ref } from "vue";
+const drawer = ref(false)
 
-import { ArrowRight, ArrowLeft, Setting} from "@element-plus/icons-vue";
-import {ref} from "vue";
-const circleUrl = ref('https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png')
+const circleUrl = ref("https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png");
+
+const onBack = () => {
+  console.log("返回按钮被点击");
+};
 </script>
 
 <style scoped>
+@import '@/assets/font/font.css';
+
+/* 主容器的样式，与 tab 样式一致 */
 .button-container {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding-top: 10px;
+  padding: 10px 20px;
+  background-image: url('@/assets/img_4.png'); /* 设置与标签栏一致的背景 */
+  background-size: cover;
+  background-position: center;
+  border-bottom-left-radius: 30px;
+  border-bottom-right-radius: 30px;
+}
+
+/* 返回按钮样式 */
+.header {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+}
+
+.back-icon {
+  color: #fff8f0;
+  font-size: 25px;
+  margin-right: 8px;
+}
+
+/* 标题样式 */
+.title {
+  color: #fff8f0;
+  font-family: 'HelveticaNeue', serif;
+  font-size: 20px;
+  font-weight: bold;
+}
+
+/* 用户信息区域 */
+.user-info {
+  display: flex;
+  align-items: center;
+  color: #fff8f0;
+}
+
+.sub-title {
+  font-family: 'HelveticaNeue', serif;
+  font-size: 18px;
+  color: #fff8f0;
+  margin-right: 15px;
+}
+
+.block {
+  margin-right: 15px;
+}
+.header .el-page-header__content::before,
+.header .el-page-header__content::after {
+  content: none; /* 隐藏多余的分隔符 */
+}
+/* 工具栏图标样式 */
+.toolbar .el-icon {
+  color: #fff8f0;
 }
 </style>
